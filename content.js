0a92e0eb3f6f8059023de7d713a3016f86018f31
bashir2017 <bashiralhanshali@yahoo.com>
2020-11-28 08:54:05 -0500

    published chrome extension

--- content.js
+++ content.js
@@ -7,32 +7,32 @@ var actualCode =  '(' + function(){
                 let svg = e.target.childNodes[0]
                 let counter = 1; 
                 let timer; 
-             
+
+                //change style of clap svg to green 
                 svg.style.fill="green"
                 svg.style.stroke="green"
                 
             function triggerMouseEvent (node, eventType) {
-                console.log(counter)
-                if(counter > 50) clearInterval(timer)
+                if(counter > 100) clearInterval(timer)
                 var clickEvent = document.createEvent ('MouseEvents');
                 clickEvent.initEvent (eventType, true, true);
                 node.dispatchEvent (clickEvent);
                 counter++; 
             }
-            //change style of clap svg to green 
+            
             
             // for(let i =0; i < 49; i++){
                 timer = setInterval(() => {
                     triggerMouseEvent(svg, 'mousedown')
                     triggerMouseEvent(svg, 'mouseup')
-                }, 700)
+                }, 200)
            
             // }
                 
         
             }
         } catch (error) {
-            console.log(error)
+            //no need to console an error 
         }  
         
     })
